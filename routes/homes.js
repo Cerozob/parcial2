9c017f510ed387f3d755c113b9354537b74d4e33
Cerozob <55302468+Cerozob@users.noreply.github.com>
2021-05-30 16:16:10 -0500

    yeah, cors

--- routes/homes.js
+++ routes/homes.js
@@ -2,16 +2,22 @@ var express = require("express");
 var router = express.Router();
 var jwtValidator = require("../middlewares/jwt-validator");
 var [getHomes, getHomeById] = require("../controllers/home");
+var cors = require("cors");
 
+var corsOptions = {
+	origin: false,
+	methods: "GET",
+	optionsSuccessStatus: 204,
+};
 /* GET homes listing. */
-router.get("/", async function (req, res, next) {
-  const homes = await getHomes();
-  res.send(homes);
+router.get("/", cors(corsOptions), async function (req, res, next) {
+	const homes = await getHomes();
+	res.send(homes);
 });
 /* GET home by ID */
-router.get("/:id", async function (req, res, next) {
-  const home = await getHomeById(req.params.id);
-  res.send(home);
+router.get("/:id", cors(corsOptions), async function (req, res, next) {
+	const home = await getHomeById(req.params.id);
+	res.send(home);
 });
 
 module.exports = router;
