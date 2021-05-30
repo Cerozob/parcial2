fca8d61c6ddf5e2be8485923fa9dead908f83ced
Cerozob <55302468+Cerozob@users.noreply.github.com>
2021-05-30 02:45:27 -0500

    fh

--- front/src/services/utils.js
+++ front/src/services/utils.js
@@ -1,11 +1,11 @@
 const HOME_API = "/api/homes";
 
 const getHomes = async () => {
-  return fetch(HOME_API).then(getJSON);
+	return fetch(HOME_API).then(getJSON);
 };
 
 const getHomeById = async (id) => {
-  return fetch(`${HOME_API}/${id}`).then(getJSON);
+	return fetch(`${HOME_API}/${id}`).then(getJSON);
 };
 
 const getJSON = (response) => response.json();
